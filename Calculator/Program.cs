253f3f29c63b2f2f6b52ad03328be18b66fee066
Guilherme Reichert <GuiReichert.TR@gmail.com>
2025-02-13 15:31:50 -0300

    Implementing First Test

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Calculator.Services;
+
+CalculatorService calculator = new CalculatorService();
+
+int num1 = 5;
+int num2 = 10;
+
+Console.WriteLine($"{num1} + {num2} = {calculator.Sum(num1,num2)}");
---
+++ CalculatorTests/CalculatorServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Calculator.Services;
+
+
+namespace CalculatorTests;
+public class CalculatorServiceTests
+{
+    private CalculatorService _calculatorService;
+
+    public CalculatorServiceTests()
+    {
+        _calculatorService = new CalculatorService();
+    }
+
+
+
+
+    [Fact]
+    public void MustDoSumOf10with5ThenReturn15()
+    {
+        // Arrange - Preparations for the tests 
+
+        int num1 = 5;
+        int num2 = 10;
+
+
+        // Act - Execute test
+
+        int result = _calculatorService.Sum(num1, num2);
+
+
+
+        // Assert - Validate results
+
+
+        Assert.Equal(15, result);
+
+
+        // to run test: "dotnet test" in the terminal 
+
+    }
+
+
+}
